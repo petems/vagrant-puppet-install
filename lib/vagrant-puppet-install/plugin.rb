20a52f075f731919f25f175fd5d1876bb3ffba91
Peter Souter <p.morsou@gmail.com>
2014-06-30 14:47:14 +0100

    Removing old comments
    This is MIT now anyways :+1:

--- lib/vagrant-puppet-install/plugin.rb
+++ lib/vagrant-puppet-install/plugin.rb
@@ -1,29 +1,9 @@
-#
-# Copyright (c) 2013, Seth Chisamore
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# This is a sanity check to make sure no one is attempting to install
-# this into an early Vagrant version.
 if Vagrant::VERSION < '1.1.0'
   fail 'The Vagrant Puppet Install plugin is only compatible with Vagrant 1.1+'
 end
 
 module VagrantPlugins
-  #
   module PuppetInstall
-    # @author Seth Chisamore <schisamo@opscode.com>
     class Plugin < Vagrant.plugin('2')
       name 'vagrant-puppet-install'
       description <<-DESC
