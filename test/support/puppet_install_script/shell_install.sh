2a70f9828028c9105aabb2acec6d6442ab838f20
Peter Souter <p.souter@kainos.com>
2014-08-04 13:37:20 +0100

    Puppet install uses shell...
    Need to add an option to give a provider maybe...

--- test/support/puppet_install_script/shell_install.sh
+++ test/support/puppet_install_script/shell_install.sh
@@ -2,19 +2,9 @@
 
 set -e
 
-echo "THIS IS RUNNING ON YOUR MACHINE!"
+echo "Spec test running now!"
 
 REPO_URL="http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm"
-
-if [ "$EUID" -ne "0" ]; then
-  echo "This script must be run as root." >&2
-  exit 1
-fi
-
-if which puppet > /dev/null 2>&1; then
-  echo "Puppet is already installed."
-  exit 0
-fi
 
 # Install puppet labs repo
 echo "Configuring PuppetLabs repo..."
